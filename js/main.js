936c51bdfa5f90c96406d69b2a9df97d1e845379
Sachin Ahuja <sachinahj@gmail.com>
2018-09-29 22:36:24 -0700

    fix nav bar

--- js/main.js
+++ js/main.js
@@ -16,9 +16,9 @@
 //Mobile Toggle Control
 // -------------------------------------------------------------
 
-$(function(){
+$(function() {
     var form = $(".contact-form");
-    form.on("submit", function (e) {
+    form.on("submit", function(e) {
         e.preventDefault();
         var name = form.find("input[name='name']").val();
         var email = form.find("input[name='email']").val();
@@ -32,9 +32,9 @@ $(function(){
 //Mobile Toggle Control
 // -------------------------------------------------------------
 
-$(function(){
-    var navMain = $(".collapse");
-    navMain.on("click", "a", null, function () {
+$(function() {
+    var navMain = $(".navbar-collapse");
+    $(".navbar-collapse a").on("click", function() {
         navMain.collapse('hide');
     });
 });
@@ -43,54 +43,45 @@ $(function(){
 // Counter
 // -------------------------------------------------------------
 
-(function () {
-
+(function() {
     $('.counter').counterUp({
         delay: 10,
         time: 1000
-
     });
-
 }());
 
 // -------------------------------------------------------------
 // Progress Bar
 // -------------------------------------------------------------
 
-(function () {
-
+(function() {
     $('.progress-content').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
         if (visible) {
-            $.each($('div.progress-bar'),function(){
-                $(this).css('width', $(this).attr('aria-valuenow')+'%');
+            $.each($('div.progress-bar'), function() {
+                $(this).css('width', $(this).attr('aria-valuenow') + '%');
             });
             $(this).unbind('inview');
         }
     });
     $('.rating-bar').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
         if (visible) {
-            $.each($('div.progress-bar'),function(){
-                $(this).css('width', $(this).attr('aria-valuenow')+'%');
+            $.each($('div.progress-bar'), function() {
+                $(this).css('width', $(this).attr('aria-valuenow') + '%');
             });
             $(this).unbind('inview');
         }
     });
-
 }());
-
-
 
 
 // -------------------------------------------------------------
 // EasyPieChart
 // -------------------------------------------------------------
 
-(function () {
-
+(function() {
     $('.language-skill').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
         if (visible) {
             $('.chart').easyPieChart({
-                //your configuration goes here
                 easing: 'easeOut',
                 delay: 3000,
                 scaleColor: false,
@@ -98,7 +89,6 @@ $(function(){
                 onStep: function(from, to, percent) {
                     this.el.children[0].innerHTML = Math.round(percent);
                 }
-
             });
         }
     });
@@ -109,12 +99,12 @@ $(function(){
 // MagnificPopup
 // -------------------------------------------------------------
 
-(function () {
+(function() {
     $('.portfolio-info a').magnificPopup({
-      type: 'image',
-      gallery:{
-        enabled:true
-      }
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
     });
 }());
 
@@ -127,54 +117,27 @@ $(window).scroll(function(event) {
 });
 
 $('#mainmenu li a').click(function() {
-    $('html, body').animate({scrollTop: $(this.hash).offset().top -1}, 1000);
+    $('html, body').animate({ scrollTop: $(this.hash).offset().top - 1 }, 1000);
     return false;
 });
 
 // User define function
 function Scroll() {
-    var contentTop      =   [];
-    var contentBottom   =   [];
-    var winTop      =   $(window).scrollTop();
-    var rangeTop    =   200;
-    var rangeBottom =   500;
-    $('#mainmenu').find('.scroll a').each(function(){
-        contentTop.push( $( $(this).attr('href') ).offset().top);
-        contentBottom.push( $( $(this).attr('href') ).offset().top + $( $(this).attr('href') ).height() );
+    var contentTop = [];
+    var contentBottom = [];
+    var winTop = $(window).scrollTop();
+    var rangeTop = 200;
+    var rangeBottom = 500;
+    $('#mainmenu').find('.scroll a').each(function() {
+        contentTop.push($($(this).attr('href')).offset().top);
+        contentBottom.push($($(this).attr('href')).offset().top + $($(this).attr('href')).height());
     })
-    $.each( contentTop, function(i){
-        if ( winTop > contentTop[i] - rangeTop ){
+    $.each(contentTop, function(i) {
+        if (winTop > contentTop[i] - rangeTop) {
             $('#mainmenu li.scroll')
-            .removeClass('current')
-            .eq(i).addClass('current');
+                .removeClass('current')
+                .eq(i).addClass('current');
         }
     })
 
 };
-
-// -------------------------------------------------------------
-//  Sticky Nav
-// -------------------------------------------------------------
-(function () {
-    function menuToggle(){
-        var windowWidth = $(window).width();
-        if(windowWidth > 991 ){
-            $(window).on('scroll', function(){
-                if( $(window).scrollTop()>735 ){
-                    $('.home-two .navbar').addClass('navbar-fixed-top animated fadeInDown');
-                } else {
-                    $('.home-two .navbar').removeClass('navbar-fixed-top animated fadeInDown');
-                };
-            });
-        }else{
-
-            $('.home-two .navbar').addClass('navbar-fixed-top');
-
-        };
-    }
-
-    menuToggle();
-}());
-
-
-
