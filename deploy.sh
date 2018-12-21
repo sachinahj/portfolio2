cf024eaf824a52b8aab3f1f84e46279ab5b6d571
Sachin Ahuja <sachinahj@gmail.com>
2018-12-20 21:46:40 -0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-aws s3 sync . s3://sachinahuja.info --exclude ".git/*" --exclude ".DS_Store" --exclude "deploy.sh"
+aws s3 sync . s3://www.sachinahuja.info --exclude ".git/*" --exclude ".DS_Store" --exclude "deploy.sh"
